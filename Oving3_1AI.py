9a8ec1148869183eb752b0947b7a6dadd48a7e07
hakon0601 <hakongimse@gmail.com>
2014-09-23 23:25:03 +0200

    creating 2d node list and setting up initial parents

--- Oving3_1AI.py
+++ Oving3_1AI.py
@@ -7,11 +7,13 @@ F = G + H
 import math
 
 class Node:
-	def __init__(self, h, g, wall, endPos):
+	def __init__(self, h, g, wall, endPos, parent):
 		self.h = h
-		self.g = h
+		self.g = g
+		self.f = h + g
 		self.wall = wall
 		self.endPos = endPos
+		self.parent = parent
 
 def getPosOf(element):
 	for y in range(len(board)):
@@ -36,16 +38,20 @@ def setupBoard():
   		board = f.read().splitlines()
   	return(board)
 
-def getH(x, y):
+def getManhattanH(x, y):
 	return abs(endPos[0] - x) + abs(endPos[1] - y)
+
+def getEuclidianH(x, y):
+	#TODO easy later
 
 def createInitialNodes():
 	verticalNodeList = [] 
 	for y in range(len(board)):
 		horizontalNodeList = [] 		
 		for x in range(len(board[y])):
-			h = getH(x, y)
+			h = getManhattanH(x, y)
 			g = 0
+			parent = None
 			if (board[y][x] == "." or board[y][x] == "A"):
 				wall = False
 				endPos = False
@@ -59,14 +65,36 @@ def createInitialNodes():
 		verticalNodeList.append(horizontalNodeList)
 	return verticalNodeList	
 
-		
 
-board = setupBoard()
-startPos = getPosOf("A")
-endPos = getPosOf("B")
-node2dList = createInitialNodes()
+def setupInitialParents():
+	if startPos[0] != 0: 
+		# Adding startnode as parent of the west-node
+		node2dList[startPos[1]][startPos[0] - 1].parent = startNode
+	if startPos[1] != 0:
+		#North
+		node2dList[startPos[1] - 1][startPos[0]].parent = startNode
+	if startPos[1] < (len(node2dList) - 1):
+		#South
+		node2dList[startPos[1] + 1][startPos[0]].parent = startNode
+	if startPos[0] < (len(node2dList[startPos[1]]) - 1):
+		#East
+		node2dList[startPos[1]][startPos[0] + 1].parent = startNode
+		
+board = setupBoard() # (y, x)
+startPos = getPosOf("A") # (x, y)
+endPos = getPosOf("B") # (x, y)
+node2dList = createInitialNodes() # (y, x)...
+startNode = node2dList[startPos[1]][startPos[0]]
 
 printBoard()
 
 closedList = []
 openList = []
+
+closedList.append(startNode) #Adding the startnode to the closed list
+
+
+
+
+
+

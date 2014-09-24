9997e7166c1728a59a7b090117711930d835b653
hakon0601 <hakongimse@gmail.com>
2014-09-24 23:06:06 +0200

    finished A star task 1 subproblem 1

--- Oving3_1AI.py
+++ Oving3_1AI.py
@@ -7,10 +7,10 @@ F = G + H
 import math
 
 class Node:
-	def __init__(self, h, g, wall, endPos, parent):
+	def __init__(self, pos, h, g, wall, endPos, parent):
+		self.pos = pos
 		self.h = h
 		self.g = g
-		self.f = h + g
 		self.wall = wall
 		self.endPos = endPos
 		self.parent = parent
@@ -33,15 +33,15 @@ def printHs():
 		print(horizontalHs)
 
 def setupBoard():
-	fname = 'board-1-1.txt'
+	fname = 'board-1-4.txt'
 	with open(fname) as f:
-  		board = f.read().splitlines()
-  	return(board)
+		board = f.read().splitlines()
+	return(board)
 
 def getManhattanH(x, y):
 	return abs(endPos[0] - x) + abs(endPos[1] - y)
 
-def getEuclidianH(x, y):
+#def getEuclidianH(x, y):
 	#TODO easy later
 
 def createInitialNodes():
@@ -50,7 +50,7 @@ def createInitialNodes():
 		horizontalNodeList = [] 		
 		for x in range(len(board[y])):
 			h = getManhattanH(x, y)
-			g = 0
+			g = 10000 # TODO fix
 			parent = None
 			if (board[y][x] == "." or board[y][x] == "A"):
 				wall = False
@@ -61,37 +61,107 @@ def createInitialNodes():
 			else:
 				wall = False
 				endPos = True
-			horizontalNodeList.append(Node(h, g, wall, endPos))
+			horizontalNodeList.append(Node((x, y), h, g, wall, endPos, parent))
 		verticalNodeList.append(horizontalNodeList)
 	return verticalNodeList	
 
 
-def setupInitialParents():
-	if startPos[0] != 0: 
-		# Adding startnode as parent of the west-node
-		node2dList[startPos[1]][startPos[0] - 1].parent = startNode
-	if startPos[1] != 0:
-		#North
-		node2dList[startPos[1] - 1][startPos[0]].parent = startNode
-	if startPos[1] < (len(node2dList) - 1):
-		#South
-		node2dList[startPos[1] + 1][startPos[0]].parent = startNode
-	if startPos[0] < (len(node2dList[startPos[1]]) - 1):
-		#East
-		node2dList[startPos[1]][startPos[0] + 1].parent = startNode
-		
+def setupChildrenOfNode(parentNode):
+	if parentNode.pos[0] != 0: 
+		westNode = node2dList[parentNode.pos[1]][parentNode.pos[0] - 1]
+		reCheckNode(westNode, parentNode)
+		if (westNode.endPos):
+			return westNode
+
+	if parentNode.pos[1] != 0:
+		northNode = node2dList[parentNode.pos[1] - 1][parentNode.pos[0]]
+		reCheckNode(northNode, parentNode)
+		if (northNode.endPos):
+			return northNode
+
+	if parentNode.pos[1] < (len(node2dList) - 1):
+		southNode = node2dList[parentNode.pos[1] + 1][parentNode.pos[0]]
+		reCheckNode(southNode, parentNode)
+		if (southNode.endPos):
+			return southNode
+
+	if parentNode.pos[0] < (len(node2dList[parentNode.pos[1]]) - 1):
+		eastNode = node2dList[parentNode.pos[1]][parentNode.pos[0] + 1]
+		reCheckNode(eastNode, parentNode)
+		if (eastNode.endPos):
+			return (eastNode)
+	return None
+
+def reCheckNode(node, parentNode):
+	if (node.wall == False and (node not in closedList)): 
+			if ((parentNode.g + 1) < node.g):
+				node.parent = parentNode
+				node.g = 1 + parentNode.g
+			openList.append(node)
+
+def getBestNodeInOpenList():
+	bestNode = openList[0]
+	for node in openList:
+		if (node.g + node.h) < (bestNode.g + bestNode.h):
+			bestNode = node
+	return bestNode
+
+def printLists():
+	oListPos = []
+	for pos in openList:
+		oListPos.append(pos.pos)
+	print(oListPos)
+	cListPos = []
+	for poss in closedList:
+		cListPos.append(poss.pos)
+	print(cListPos)
+
+
 board = setupBoard() # (y, x)
 startPos = getPosOf("A") # (x, y)
 endPos = getPosOf("B") # (x, y)
 node2dList = createInitialNodes() # (y, x)...
 startNode = node2dList[startPos[1]][startPos[0]]
+startNode.g = 0
 
 printBoard()
 
 closedList = []
 openList = []
 
-closedList.append(startNode) #Adding the startnode to the closed list
+openList.append(startNode) #Adding the startnode to the closed list
+
+#printLists()
+endNode = None
+
+while(True):
+	bestNode = getBestNodeInOpenList()
+	openList.remove(bestNode)
+	closedList.append(bestNode)
+	endNode = setupChildrenOfNode(bestNode)
+	if (endNode != None):
+		break
+
+print("\n")
+
+# Altering and printing board. Comma represents the path
+node = endNode
+while (node.parent != None):
+	s = list(board[node.pos[1]])
+	s[node.pos[0]] = ","
+	board[node.pos[1]] = "".join(s)
+	node = node.parent
+
+s = list(board[endPos[1]])
+s[endPos[0]] = "B"
+board[endPos[1]] = "".join(s)
+
+printBoard()
+
+
+
+
+
 
 
 

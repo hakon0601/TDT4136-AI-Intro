7502d4826ae29ff25d256754f2e1f0f6409e78c1
talepre <tale.prestmo@gmail.com>
2014-09-26 13:55:47 +0200

    Commented out the code

--- Oving3_1AI.py
+++ Oving3_1AI.py
@@ -15,16 +15,19 @@ class Node:
 		self.endPos = endPos
 		self.parent = parent
 
+#This function gets position of given ?
 def getPosOf(element):
 	for y in range(len(board)):
 		for x in range(len(board[y])):
 			if (board[y][x] == element):
 				return (x, y)
 
+#This prints the board at a given time
 def printBoard():
 	for line in board:
 		print(line)
 
+#This prints 
 def printHs():
 	for y in range(len(board)):
 		horizontalHs = []
@@ -32,18 +35,21 @@ def printHs():
 			horizontalHs.append(node2dList[y][x].h)
 		print(horizontalHs)
 
+#This sets up the board as it is given in the textfils
 def setupBoard():
 	fname = 'board-1-1.txt'
 	with open(fname) as f:
 		board = f.read().splitlines()
 	return(board)
 
+#This function computes the Manhatten distance from A to B
 def getManhattanH(x, y):
 	return abs(endPos[0] - x) + abs(endPos[1] - y)
 
 #def getEuclidianH(x, y):
-	#TODO easy later
+	#TODO easy later/not needed, we use manhattan distance instead
 
+#This function creates all the initial nodes and puts it in a list system, depending on if its a wall, the start point, end point, or just open nodes.
 def createInitialNodes():
 	verticalNodeList = [] 
 	for y in range(len(board)):
@@ -58,33 +64,37 @@ def createInitialNodes():
 			elif (board[y][x] == "#"):
 				wall = True
 				endPos = False
-			else:
+			elif (board[y][x] == "B"):
 				wall = False
 				endPos = True
 			horizontalNodeList.append(Node((x, y), h, g, wall, endPos, parent))
 		verticalNodeList.append(horizontalNodeList)
 	return verticalNodeList	
 
-
+#This funstion gives each node its children, on each side of the node
 def setupChildrenOfNode(parentNode):
+	#sets up the children to the left(west)
 	if parentNode.pos[0] != 0: 
 		westNode = node2dList[parentNode.pos[1]][parentNode.pos[0] - 1]
 		reCheckNode(westNode, parentNode)
 		if (westNode.endPos):
 			return westNode
 
+	#sets up the children upwards(north)
 	if parentNode.pos[1] != 0:
 		northNode = node2dList[parentNode.pos[1] - 1][parentNode.pos[0]]
 		reCheckNode(northNode, parentNode)
 		if (northNode.endPos):
 			return northNode
 
+	#sets up the children downwards(south)		
 	if parentNode.pos[1] < (len(node2dList) - 1):
 		southNode = node2dList[parentNode.pos[1] + 1][parentNode.pos[0]]
 		reCheckNode(southNode, parentNode)
 		if (southNode.endPos):
 			return southNode
 
+	#sets up the children to the right(east)		
 	if parentNode.pos[0] < (len(node2dList[parentNode.pos[1]]) - 1):
 		eastNode = node2dList[parentNode.pos[1]][parentNode.pos[0] + 1]
 		reCheckNode(eastNode, parentNode)
@@ -92,6 +102,7 @@ def setupChildrenOfNode(parentNode):
 			return (eastNode)
 	return None
 
+#Checks node that hasent been discovered yet
 def reCheckNode(node, parentNode):
 	if (node.wall == False and (node not in closedList and node not in openList)): 
 			if ((parentNode.g + 1) < node.g):
@@ -99,6 +110,7 @@ def reCheckNode(node, parentNode):
 				node.g = 1 + parentNode.g
 			openList.append(node)
 
+#Goes through the list of all discovered nodes and picks the one with the lowes cost
 def getBestNodeInOpenList():
 	bestNode = openList[0]
 	for node in openList:
@@ -106,6 +118,7 @@ def getBestNodeInOpenList():
 			bestNode = node
 	return bestNode
 
+#Prints the open and closed list we have made. Openlist are nodes who have been discovered but not visited, and closedlist ar nodes you once visited because you thought it was the best
 def printLists():
 	oListPos = []
 	for pos in openList:
@@ -116,7 +129,7 @@ def printLists():
 		cListPos.append(poss.pos)
 	print(cListPos)
 
-
+#makes the board with all nodes
 board = setupBoard() # (y, x)
 startPos = getPosOf("A") # (x, y)
 endPos = getPosOf("B") # (x, y)
@@ -134,6 +147,7 @@ openList.append(startNode) #Adding the startnode to the closed list
 #printLists()
 endNode = None
 
+#moves the best node from openlist and appends it in the closedlist
 while(True):
 	bestNode = getBestNodeInOpenList()
 	openList.remove(bestNode)
